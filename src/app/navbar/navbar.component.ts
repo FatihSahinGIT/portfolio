cfa75f0a20b04b2182cb212e990b10e5d17c3f9f
Fatih Sahin <s73121@beuth-hochschule.de>
2025-06-27 21:31:15 +0200

    Update nav routing and replacing logo with svg

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { AfterViewInit, Component, ElementRef, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { gsap } from 'gsap/gsap-core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
+  imports: [RouterLink],
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent implements AfterViewInit {
