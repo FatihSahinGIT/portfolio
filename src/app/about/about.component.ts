04cad9e6fc9df62473e164c90aee6d379d4b0f06
Fatih Sahin <s73121@beuth-hochschule.de>
2025-06-29 18:03:16 +0200

    Add education

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,13 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { gsap } from 'gsap/gsap-core';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-about',
@@ -11,6 +19,9 @@ export class AboutComponent implements AfterViewInit {
   @ViewChild('imagesAbout', { static: false })
   imagesAbout!: ElementRef<HTMLElement>;
 
+  @ViewChildren('educationBlock', { read: ElementRef })
+  educationBlocks!: QueryList<ElementRef<HTMLElement>>;
+
   ngAfterViewInit(): void {
     gsap.from('.float-image', {
       opacity: 0,
@@ -20,5 +31,22 @@ export class AboutComponent implements AfterViewInit {
       duration: 2,
       ease: 'power2.out',
     });
+
+    console.log('eduaction', this.educationBlocks);
+
+    gsap.from(gsap.utils.toArray('.education-block'), {
+      opacity: 0,
+      y: 10,
+      duration: 1.2,
+      stagger: 0.3,
+      ease: 'power2.out',
+      scrollTrigger: {
+        trigger: '.education-block',
+        start: 'top 25%',
+        markers: true,
+      },
+    });
+
+    ScrollTrigger.refresh();
   }
 }
