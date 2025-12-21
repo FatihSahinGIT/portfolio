09d9c63c7bb686901c26174b36361bf949002b10
Fatih Sahin <s73121@beuth-hochschule.de>
2025-12-21 14:06:33 +0100

    Add styling to figcaption, fix contrast

--- src/app/shared/landing/selected-work/selected-work-box/selected-work.component-box.ts
+++ src/app/shared/landing/selected-work/selected-work-box/selected-work.component-box.ts
@@ -2,13 +2,12 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'selected-work-box',
-    templateUrl: './selected-work.component-box.html',
-
+    templateUrl: './selected-work-box.component.html',
+    styleUrl: './selected-work-box.component.css'
 })
 export class SelectedWorkBoxComponent {
     @Input() name: string = '';
     @Input() imageUrl: string = '';
     @Input() imageAlt: string = '';
     @Input() tools: string[] = [];
-
 }
